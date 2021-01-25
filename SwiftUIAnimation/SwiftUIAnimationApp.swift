9516b860d8162cc906854f55dc897f99c2b03030
Hua Son Tung <tunghuason@gmail.com>
2021-01-25 13:26:54 +0700

    Initial Commit

---
+++ SwiftUIAnimation/SwiftUIAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAnimationApp.swift
+//  SwiftUIAnimation
+//
+//  Created by Hua Son Tung on 25/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
